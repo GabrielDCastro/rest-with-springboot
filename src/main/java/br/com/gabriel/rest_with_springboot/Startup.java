9c8e09efe19079a8291cce0c335ebef61ac02fd2
GabrielDCastro <gabrielrangel12@gmail.com>
2024-12-18 20:02:45 -0300

    first configurations

---
+++ src/main/java/br/com/gabriel/rest_with_springboot/Startup.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.rest_with_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabriel/rest_with_springboot/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.rest_with_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
